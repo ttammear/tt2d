3e6d56fa1ce31cdd87d1f6c5f77bae0f50aac9a9
talis <talis@tammearu.eu>
2016-12-20 20:31:11 +0200

    added transformsystem and moved related code into it

---
+++ transformsystem.cpp
@@ -0,0 +1,6 @@
+#include "transformsystem.h"
+
+TransformSystem::TransformSystem()
+{
+
+}
---
+++ transformsystem.h
@@ -0,0 +1,11 @@
+#ifndef TRANSFORMSYSTEM_H
+#define TRANSFORMSYSTEM_H
+
+
+class TransformSystem
+{
+public:
+    TransformSystem();
+};
+
+#endif // TRANSFORMSYSTEM_H
